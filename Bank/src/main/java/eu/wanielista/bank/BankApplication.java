1757159e2ca134c594b54524e579cd421567e323
mwanielista <michal@wanielista.eu>
2021-06-21 19:49:52 +0200

    Added new projects structure

---
+++ Bank/src/main/java/eu/wanielista/bank/BankApplication.java
@@ -0,0 +1,13 @@
+package eu.wanielista.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankApplication.class, args);
+    }
+
+}
